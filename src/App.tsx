808c4437aed91c9fea2ebee8300b1c6f7b52f638
Gustavo Salviato <75762976+gustavosalviato@users.noreply.github.com>
2022-10-20 13:11:41 -0300

    Configuração do CreateGlobalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { Button } from "./components/Button"
 import { ThemeProvider } from "styled-components"
 import { defaultTheme } from "./styles/themes/default"
+import { GlobalStyle } from "./styles/themes/global"
 
 export const App = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" size="lg" />
+      Teste
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
