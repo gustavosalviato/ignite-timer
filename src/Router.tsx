f3be13ea0413a9743c1d69f834bb492b609add17
gustavosalviato <gustavosalviato8@gmail.com>
2022-10-20 21:45:33 -0300

    Estilização do layout

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import { Home } from './Pages/Home'
 import { History } from './Pages/History'
-import { DefaultLayout } from './layouts/DefaultLayout'
+import { DefaultLayout } from './layouts/DefaultLayout/DefaultLayout'
 
 export const Router = () => {
     return (
