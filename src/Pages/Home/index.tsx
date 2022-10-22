c1c1436a1dc1a2570014df8e1384416ceef3cf35
gustavosalviato <gustavosalviato8@gmail.com>
2022-10-22 09:39:03 -0300

    Hook useForm

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,16 +1,35 @@
 import { Play } from "phosphor-react"
-import { HomeContainer, CountDownContainer, FormContainer, Separator, ButtonContainer, BasicInput, MinutesAmoutInput, TaskInput } from "./styles"
-
+import { HomeContainer, CountDownContainer, FormContainer, Separator, ButtonContainer, MinutesAmoutInput, TaskInput } from "./styles"
+import { useForm } from 'react-hook-form'
 export const Home = () => {
+
+  const { handleSubmit, register, watch } = useForm()
+
+  const handleCreateNewCycle = (data: any) => {
+    console.log(data)
+  }
+
+  const task = watch('task')
+
+  const isSubmitDisabled = !task
+
   return (
     <HomeContainer>
-      <form>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput type="text" placeholder="Dê um nome para seu projeto" />
+          <TaskInput
+            type="text"
+            placeholder="Dê um nome para seu projeto"
+            {...register('task')}
+          />
 
           <label htmlFor="task">durante</label>
-          <MinutesAmoutInput type="text" placeholder="00" />
+          <MinutesAmoutInput
+            type="text"
+            placeholder="00"
+            {...register('minutesAmount')}
+          />
 
           <span>Minutos</span>
         </FormContainer>
@@ -18,12 +37,12 @@ export const Home = () => {
         <CountDownContainer>
           <span>0</span>
           <span>0</span>
-          <Separator></Separator>
+          <Separator>:</Separator>
           <span>0</span>
           <span>0</span>
         </CountDownContainer>
 
-        <ButtonContainer disabled>
+        <ButtonContainer disabled={isSubmitDisabled}>
           <Play size={24} />
           Começar
         </ButtonContainer>
